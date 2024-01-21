da459dbb0e6fc3dc6477e2ee209acbcb484a2954
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-21 16:15:24 -0300

    Removendo coisas desnecessarios e fazendo ajustes

--- src/infra/repositories/repositories.module.ts
+++ src/infra/repositories/repositories.module.ts
@@ -3,36 +3,10 @@ import { TypeOrmConfigModule } from '../config/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteEntity } from '../entities/cliente.entity';
 import { ClienteRepositoryImpl } from './cliente.repository.impl';
-import { ProdutoEntity } from '../entities/produto.entity';
-import { ProdutoRepositoryImpl } from './produto.repository.impl';
-import { PedidoEntity } from '../entities/pedido.entity';
-import { PedidoRepositoryImpl } from './pedido.repository.impl';
-import { ItemPedidoEntity } from '../entities/item-pedido.entity';
-import { PagamentoEntity } from '../entities/pagamento.entity';
-import { PagamentoRepositoryImpl } from './pagamento.repository.impl';
 
 @Module({
-  imports: [
-    TypeOrmConfigModule,
-    TypeOrmModule.forFeature([
-      ClienteEntity,
-      ProdutoEntity,
-      PedidoEntity,
-      ItemPedidoEntity,
-      PagamentoEntity,
-    ]),
-  ],
-  providers: [
-    ClienteRepositoryImpl,
-    ProdutoRepositoryImpl,
-    PedidoRepositoryImpl,
-    PagamentoRepositoryImpl,
-  ],
-  exports: [
-    ClienteRepositoryImpl,
-    ProdutoRepositoryImpl,
-    PedidoRepositoryImpl,
-    PagamentoRepositoryImpl,
-  ],
+  imports: [TypeOrmConfigModule, TypeOrmModule.forFeature([ClienteEntity])],
+  providers: [ClienteRepositoryImpl],
+  exports: [ClienteRepositoryImpl],
 })
 export class RepositoriesModule {}
